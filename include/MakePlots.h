2536e2d381234f84ddf274494b35731d642ecc2d
Chia-hung-tw <chchien521@gmail.com>
2018-11-11 06:03:41 +0100

    Putting TProfile_Maker read/write function into makeplot.

--- include/MakePlots.h
+++ include/MakePlots.h
@@ -3,8 +3,10 @@
 
 #include "setup_config.h"
 #include "TProfile.h"
-
-
+#include "TFile.h"
+#include "TTree.h"
+#include <string>
+using namespace std;
 class MakePlots{
  public:
   MakePlots();
@@ -13,10 +15,25 @@ class MakePlots{
   //TProfile members
   TProfile *HG_LG [MAXBOARDS][MAXSKI][MAXCH];
   TProfile *LG_TOT[MAXBOARDS][MAXSKI][MAXCH];
-
-  //Function
-  void Init_TProfile(int BD); //Init a module (4*32plots)
+ 
+  // Function
+  void Init_TFile(string TPro_outputname);
+  void Write_TProfile();  // Takes time, fill every few runs
+  bool Check_Run(int RunN);
+  
+  // member
+  bool file_exist;
+  int  history_Run;
+  string TPro_output;
+  
+  
+ private:
+  // Function
   void Init_Pointers();
+
+  TFile *TPro_root;
+  TTree *TPro_history;
+  
 };
 
 #endif
